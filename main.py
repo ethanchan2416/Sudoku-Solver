48403b1ef22fcdc5d53243aead9469f9c9b0be3a
ethanchan2416 <ethanchan2416@gmail.com>
2020-03-28 14:50:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,12 @@ TestMatrix = ((0, 8, 0, 0, 5, 0, 2, 0, 0),
               (9, 0, 0, 0, 4, 8, 0, 0, 7),
               (0, 0, 0, 9, 2, 0, 4, 0, 0))
 
-solve_sudoku(TestMatrix, 'row')
+print('Manually enter testset, PRESS 1')
+print('Use sample testset, PRESS 2\n')
+choice = input('Enter choice: ')
+print()
+if choice is '1':
+    testset = input_testset()
+elif choice is '2':
+    testset = TestMatrix
+solve_sudoku(testset, 'row')
